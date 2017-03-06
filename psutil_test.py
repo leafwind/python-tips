c1a5b33d98c8843fc4201de095548590badfbf99
leafwind <leafwind.cs@gmail.com>
2017-03-06 14:52:19 +0800

    update

--- psutil_test.py
+++ psutil_test.py
@@ -1,3 +1,9 @@
+"""print the memory info of this machine"""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
 import psutil
-print psutil.virtual_memory()
-print psutil.swap_memory()
+
+print(psutil.virtual_memory())
+print(psutil.swap_memory())
